9adba4a8498e82556dc8dd17fe0035db5a40f416
dymaxionkim <dymaxion.kim@gmail.com>
2019-06-28 16:51:26 +0900

    docx

--- resume.sh
+++ resume.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-pandoc resume.md --pdf-engine=xelatex -o resume.pdf
+pandoc resume.md -s --pdf-engine=xelatex -o resume.pdf
+pandoc resume.md -s --highlight-style tango -f markdown+multiline_tables+grid_tables+pipe_tables+table_captions --reference-doc=resume_format.docx -o resume.docx
